de2272b012fe47853e40f3f28b4a99eb21ef1bcd
Priyanka Ghimire <priyankaghimire42@gmail.com>
2024-06-08 21:39:17 +0545

    Day 8

---
+++ Day 8/test.js
@@ -0,0 +1,33 @@
+console.log('Hello world');
+let name="Steve";
+console.log(name);
+let value=10;
+value=5;
+console.log(value);
+const interest=10;
+console.log(interest);
+let age= 50;
+let isApproved= false;
+let firstName= undefined;
+let selector= null;
+console.log(typeof name);
+let student={
+    level: "Undergraduate",
+    major: "Computer science"
+};
+console.log(student);
+student.major="literature";
+console.log(student.major);
+let product =["bike","cycle"];
+console.log(product);
+console.log(product[1]);
+product[2]="taxi";
+console.log(product);
+product[3]=8;
+console.log(product.length);
+hi("Mary");
+function hi(name){
+    console.log("Hello "+name);
+}
+
+
