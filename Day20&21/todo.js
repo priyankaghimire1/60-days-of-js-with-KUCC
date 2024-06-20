97cf3c4200a9b583c05269b893308ef397f73566
Priyanka Ghimire <priyankaghimire42@gmail.com>
2024-06-20 22:18:51 +0545

    Day 20

---
+++ Day20&21/todo.js
@@ -0,0 +1,9 @@
+const input =document.getElementById("input");
+const container =document.getElementById("todo");
+function task(){
+    const element=input.value.trim();
+    if (!task){
+        alert('Please write down something');
+        return;
+    }
+}
