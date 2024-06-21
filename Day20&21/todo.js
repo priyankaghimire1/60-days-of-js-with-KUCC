4bffaca5cbd170c09f17d973dc2b884797db9615
Priyanka Ghimire <priyankaghimire42@gmail.com>
2024-06-21 21:58:34 +0545

    Day 21

--- Day20&21/todo.js
+++ Day20&21/todo.js
@@ -1,9 +1,17 @@
 const input =document.getElementById("input");
 const container =document.getElementById("todo");
-function task(){
-    const element=input.value.trim();
-    if (!task){
-        alert('Please write down something');
-        return;
-    }
+const list = document.querySelector('ul');
+list.addEventListener('click', function(ev){
+  if(ev.target.tagName ==='LI'){
+    ev.target.classList.toggle('checked');
+  }
+}, false)
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("input").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    document.getElementById("todo").appendChild(li);
+    document.getElementById("input").value = "";
 }
+  
