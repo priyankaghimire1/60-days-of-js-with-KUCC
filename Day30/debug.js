c0b854cde7382eed156151079d56621e2fcd62b6
Priyanka Ghimire <priyankaghimire42@gmail.com>
2024-06-30 20:02:46 +0545

    Day30

---
+++ Day30/debug.js
@@ -0,0 +1,10 @@
+function logSomething(){
+    console.log(Math.random());
+}
+function warn(){
+    console.warn("This is a warning");
+}
+console.error("This is an error.");
+const styles=
+"background-color: #ebc7e2;padding: 5%;";
+console.log('%cHello', styles);
