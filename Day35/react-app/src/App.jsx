5a575c68d7e8d3bc6e501d724cff3977aca5ec74
Priyanka Ghimire <priyankaghimire42@gmail.com>
2024-07-05 22:41:16 +0545

    Day 35

---
+++ Day35/react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to the page</h1>
+      <ul>Fruits<li>Apples</li>
+      <li>Mango</li></ul>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel massa id sem sodales tincidunt in nec tortor. Etiam mi diam, consectetur nec est sit amet, placerat venenatis metus. Cras tincidunt metus purus, sit amet feugiat augue molestie ut. Suspendisse nec eros quis eros feugiat posuere id id justo. Morbi ac turpis sed magna varius finibus eu sed ante. Duis hendrerit eget turpis ac rutrum. Nulla congue consequat suscipit.</p>
+    </>
+  )
+}
+
+export default App
